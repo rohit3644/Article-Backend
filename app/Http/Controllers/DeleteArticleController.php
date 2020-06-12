6aecf8d4f7a7d421c73d664ba8380a305a862982
Rohit <rohit.kumar@mindfiresolutions.com>
2020-06-13 00:34:52 +0530

    Removed cors middleware from backend

--- app/Http/Controllers/DeleteArticleController.php
+++ app/Http/Controllers/DeleteArticleController.php
@@ -5,24 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Article;
 use Exception;
+use Illuminate\Support\Facades\File;
 
 class DeleteArticleController extends Controller
 {
     public function delete(Request $req)
     {
         try {
+            $article = Article::find($req->id);
+            File::delete('upload/images/' . $article->image_name);
             $article = Article::find($req->id)->delete();
+
             return response()->json([
-                "message" => "Successfully deleted the article",
                 "code" => 200,
             ]);
         } catch (Exception $e) {
             return response()->json([
-                "message" => "Error in deleting the article",
-                "code" => 201,
+                "code" => 419,
             ]);
         }
-
-        // return "Hi";
     }
 }
