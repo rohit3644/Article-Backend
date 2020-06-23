187ecd82240e89720049cbd078ce90ac74448d64
Rohit <rohit.kumar@mindfiresolutions.com>
2020-06-23 13:42:03 +0530

    Changed the app_url to production domain

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://http://ec2-13-233-208-70.ap-south-1.compute.amazonaws.com'),
 
     /*
     |--------------------------------------------------------------------------
