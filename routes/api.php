5ba1184dbe0a1110d0fba97df09829c4f5a25427
Rohit <rohit.kumar@mindfiresolutions.com>
2020-06-24 13:54:28 +0530

    Updated namespace and api routes

--- routes/api.php
+++ routes/api.php
@@ -1,61 +1,61 @@
 <?php
 
 // get article
-Route::get('/article', 'ArticleController@index');
+Route::get('/article', '/Articles/ArticleController@index');
 
 // send otp
-Route::post('/otp-send', 'OTPSendController@send');
+Route::post('/otp-send', '/Twilio/OTPSendController@send');
 
 // verify otp
-Route::post('/otp-verify', 'OTPVerifyController@verify');
+Route::post('/otp-verify', '/Twilio/OTPVerifyController@verify');
 
 // registration
-Route::post('/register', 'RegistrationController@register');
+Route::post('/register', '/Registration/RegistrationController@register');
 
 // login
-Route::post('/login', 'LoginController@login');
+Route::post('/login', '/Login/LoginController@login');
 
 // update article
-Route::post('/update-article', 'UpdateArticleController@update')->middleware("authKey");
+Route::post('/update-article', '/Articles/UpdateArticleController@update')->middleware("authKey");
 
 // delete article
-Route::post('/delete-article', 'DeleteArticleController@delete')->middleware("authKey");
+Route::post('/delete-article', '/Articles/DeleteArticleController@delete')->middleware("authKey");
 
 // readmore article
-Route::post('/read-more', 'ReadMoreController@getId');
+Route::post('/read-more', '/Articles/ReadMoreController@getId');
 
 // get the article data to prepopulate update field
-Route::post('/get-article', 'GetArticleController@get')->middleware("authKey");
+Route::post('/get-article', '/Articles/GetArticleController@get')->middleware("authKey");
 
 // all articles for admin screen
-Route::get('/all-articles', 'AllArticlesController@get');
+Route::get('/all-articles', '/Articles/AllArticlesController@get');
 
 // stripe payment
-Route::post('/payment', 'PaymentController@payment');
+Route::post('/payment', '/Stripe/PaymentController@payment');
 
 // add article
-Route::post('/add-article', 'AddArticleController@addArticle');
+Route::post('/add-article', '/Articles/AddArticleController@addArticle');
 
 // add comments
-Route::post('/add-comment', 'CommentController@add');
+Route::post('/add-comment', '/Comments/CommentController@add');
 
 // contact us
-Route::post('/contact', 'ContactController@contact');
+Route::post('/contact', '/Contact/ContactController@contact');
 
 
 // admin specific routes
 
-Route::post('/approve-article', 'ApproveArticleController@approve')->middleware("authKey", "admin");
+Route::post('/approve-article', '/Articles/ApproveArticleController@approve')->middleware("authKey", "admin");
 
-Route::post('/edit-comment', 'EditCommentController@edit')->middleware("authKey", "admin");
+Route::post('/edit-comment', '/Comments/EditCommentController@edit')->middleware("authKey", "admin");
 
-Route::post('/delete-comment', 'DeleteCommentController@delete')->middleware("authKey", "admin");
+Route::post('/delete-comment', '/Comments/DeleteCommentController@delete')->middleware("authKey", "admin");
 
-Route::post('/approve-comment', 'ApproveCommentController@approve')->middleware("authKey", "admin");
+Route::post('/approve-comment', '/Comments/ApproveCommentController@approve')->middleware("authKey", "admin");
 
 // user specific routes
 
-Route::post('/user-article', 'UserArticleController@index')->middleware("authKey", "user");
+Route::post('/user-article', '/Articles/UserArticleController@index')->middleware("authKey", "user");
 
 // google login
-Route::post('/googleAuth', 'LoginController@googleAuth');
+Route::post('/googleAuth', '/Login/LoginController@googleAuth');
