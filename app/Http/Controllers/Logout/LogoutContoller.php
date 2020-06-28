b332cd9f448c1274cf51b8c2b2e68e681f735a36
Rohit <rohit.kumar@mindfiresolutions.com>
2020-06-28 17:33:38 +0530

    Fixing composer issue

--- app/Http/Controllers/Logout/LogoutContoller.php
+++ app/Http/Controllers/Logout/LogoutContoller.php
@@ -14,28 +14,29 @@ class LogoutContoller extends Controller
 {
     public function logout(Request $req)
     {
+        return $req;
         // Begin Transaction
-        DB::beginTransaction();
-        try {
-            $response = new Response();
-            $id = intval(substr($req->apiToken, 65));
-            $token = Token::where('user_id', $id)
-                ->where('api_token', $req->apiToken)
-                ->where('is_active', "Yes")->get();
-            $token->is_active = "No";
-            $token->save();
-            // Commit Transaction
-            DB::commit();
-            // response
-            $msg = $response->response(200);
-            return response()->json($msg);
-        } catch (Exception $e) {
-            // Rollback Transaction
-            DB::rollback();
-            $msg = $response->response(500);
-            // Logging exception
-            Log::error($e->getMessage());
-            return response()->json($msg);
-        }
+        // DB::beginTransaction();
+        // try {
+        //     $response = new Response();
+        //     $id = intval(substr($req->apiToken, 65));
+        //     $token = Token::where('user_id', $id)
+        //         ->where('api_token', $req->apiToken)
+        //         ->where('is_active', "Yes")->get();
+        //     $token->is_active = "No";
+        //     $token->save();
+        //     // Commit Transaction
+        //     DB::commit();
+        //     // response
+        //     $msg = $response->response(200);
+        //     return response()->json($msg);
+        // } catch (Exception $e) {
+        //     // Rollback Transaction
+        //     DB::rollback();
+        //     $msg = $response->response(500);
+        //     // Logging exception
+        //     Log::error($e->getMessage());
+        //     return response()->json($msg);
+        // }
     }
 }
